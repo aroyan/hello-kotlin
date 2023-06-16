36362e972704289f1165bfd0e99f7034037cc43a
aroyan <abssneakers@gmail.com>
2023-06-16 15:48:18 +0800

    Debugging 🐛

---
+++ src/main/kotlin/debugging/breakpoints.kt
@@ -0,0 +1,17 @@
+package debugging
+
+import kotlin.random.Random
+
+fun getRandomInt(): Int {
+    return Random.nextInt()
+}
+fun main() {
+    for (i in 0 .. 4){
+        val random = getRandomInt()
+        val isEven = random.rem(2) == 0
+        when(isEven) {
+            true -> println("$random is even")
+            false -> println("$random is odd")
+        }
+    }
+}
